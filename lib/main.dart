672665e3e2dee422ba3e15f077558589506a0e20
Prasad Kanade <65277692+prasad0411@users.noreply.github.com>
2023-07-14 18:48:56 +0530

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,23 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp()); 
 
 class XylophoneApp extends StatelessWidget {
-  void sound(int num)
-  {
-  final player =  AudioCache();
-  player.play('note$num.wav');
+  // Function to play the sound
+  void sound(int num) {
+    final player = AudioCache();
+    player.play('note$num.wav');
   }
 
-  Expanded func( {Color color, int num})
-  {
+  // Function to create the buttons
+  Expanded func({Color color, int num}) {
     return Expanded(
-      // ignore: deprecated_member_use
-      // ignore: missing_required_param
-      child:FlatButton(
+      child: FlatButton(
         color: color,
-        onPressed:()
-          {
-           sound(num);
-           },
+        onPressed: () {
+          sound(num);
+        },
       ),
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -33,43 +29,28 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Column( 
-
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-
-            children:[ 
-            func(color: Colors.red, num: 1),
-            SizedBox(
-              height:5
-            ),
-            func(color: Colors.yellow, num: 2),
-            SizedBox(
-              height:5
-            ),
-            func(color: Colors.green, num: 3),
-            SizedBox(
-              height:5
-            ),
-            func(color: Colors.purple, num: 4),
-            SizedBox(
-              height:5
-            ),
-            func(color: Colors.teal, num: 5),
-            SizedBox(
-              height:5
-            ),
-            func(color: Colors.orange, num: 6),
-            SizedBox(
-              height:5
-            ),
-            func(color: Colors.blue, num: 7),
-            SizedBox(
-              height:5
-            ),
+            children: [
+              // Create buttons for each note
+              func(color: Colors.red, num: 1),
+              SizedBox(height: 5),
+              func(color: Colors.yellow, num: 2),
+              SizedBox(height: 5),
+              func(color: Colors.green, num: 3),
+              SizedBox(height: 5),
+              func(color: Colors.purple, num: 4),
+              SizedBox(height: 5),
+              func(color: Colors.teal, num: 5),
+              SizedBox(height: 5),
+              func(color: Colors.orange, num: 6),
+              SizedBox(height: 5),
+              func(color: Colors.blue, num: 7),
+              SizedBox(height: 5),
             ],
-          )
-        ) 
-      )
+          ),
+        ),
+      ),
     );
   }
 }
